6a65a9f3287c1e38745b14ca56044e0cdde8225c
Dmitriy <36514443+Lafore17@users.noreply.github.com>
2018-12-03 15:29:05 +0300

    Update kovtun.js

--- lecture 9/home/static/kovtun.js
+++ lecture 9/home/static/kovtun.js
@@ -2,9 +2,9 @@ const copied = document.getElementById('copied');
 
 copied.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET','/kovtun/copy')
+  xhr.open('GET','/kovtun/copy');
   xhr.addEventListener('load',() => {
-    alert(xhr.response)
+    alert(xhr.response);
   })
-  xhr.send(null)
+  xhr.send(null);
 })
