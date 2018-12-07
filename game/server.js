637eff5211fa55810d6b2ec2c999cbaf5802ada8
DerevishkoAndrey <derevishko@gmail.com>
2018-12-08 00:12:32 +0300

    start server

---
+++ game/server.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const mongo = require("mongodb").MongoClient;
+const PORT = 3000;
+const bodyParser = require("body-parser");
+const url = "mongodb://localhost:27017/test";
+
+app.use(bodyParser.json());
+app.use("/static", express.static("static"))
+
+app.get('/',(req,res) => {
+  console.log(1)
+    res.send(fs.readFileSync('index.html','utf8'))
+});
+
+app.post('/registration', (req,res) => {
+  if (req.body) {
+    res.send('ok');
+  } else {
+    res.send('error');
+  }
+});
+
+app.get('/login', (req,res) => {
+  if (req.body) {
+    res.send(JOSN.stringify({_id: 12312312321}));
+  } else {
+    res.send('error');
+  }
+})
+
+app.get('/start', (req,res) => {
+  res.send(JOSN.stringify({token: 'safasdgdhbfddfghjkoijuhgfdfgh'}));
+})
+
+app.post('/finish', (req,res) => {
+  if (req.body) {
+    res.send('ok');
+  } else {
+    res.send('error');
+  }
+})
+
+app.get('/records', (req,res) => {
+  let arr = new Array(100).fill({login:'fdsads',time:123213})
+  res.send(JOSN.stringify(arr));
+})
+
+app.get('/records/:id', (req,res) => {
+  let arr = new Array(100).fill({login:req.params.id,time:123213})
+  res.send(JOSN.stringify(arr));
+})
+
+
+
+app.listen(PORT,()=>console.log(PORT))
