70cc0a22b97b1811109fcf01f4aebad9c9778949
Vladioska <vlad_y99@mail.ru>
2018-12-18 21:59:20 +0300

    Integrate sass

--- news feed/webpack.config.js
+++ news feed/webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
   module: {
     rules: [
       {
+          test: /\.sass$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+      },
+      {
         test: /\.css$/,
         use: [
           'vue-style-loader',
