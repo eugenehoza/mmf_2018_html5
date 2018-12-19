7d40bf417c835df21eb68cb3a9c7ad6d6803d1bd
Vladioska <vlad_y99@mail.ru>
2018-12-19 03:30:30 +0300

    Add some styles and code refactoring

---
+++ news feed/src/components/elements/Post.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="post">
+        <div class="post-header">
+            <span class="nick">{{ post.username }}</span>
+            <time>{{ post.date }}</time>
+        </div>
+        <div class="post-body">
+            {{ post.text }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            post: Object
+        }
+    }
+</script>
